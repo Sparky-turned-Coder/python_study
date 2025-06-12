bf9c0a0f19a208c0639633fb67fbaa153290379c
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2025-06-11 20:48:17 -0500

    added strip file to test methods

---
+++ strip.py
@@ -0,0 +1,15 @@
+string = "  Hello World!  "
+
+stripped_string = string.strip()    # "Hello World!"
+lstripped_string = string.lstrip()  # "Hello World!   "
+rstripped_string = string.rstrip()  # "   Hello World!"
+
+print(f"Original string: '{string}'")
+print(f"Stripped string: '{stripped_string}'")
+print(f"Left-stripped string: '{lstripped_string}'")
+print(f"Right-stripped string: '{rstripped_string}'")
+
+string2 = "!!!Hello World!!!"
+stripped_string2 = string2.strip("!")  # "Hello World"
+print(f"Original string2: '{string2}'")
+print(f"Stripped string2 with exclamation point in arguement: '{stripped_string2}'")
