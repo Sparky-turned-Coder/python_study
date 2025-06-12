4c4603b437c56427f2dcba1d425aa48c2f13b697
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2025-06-11 20:22:29 -0500

    updated notes.md

--- name.py
+++ name.py
@@ -1,3 +1,25 @@
-name = "ada lovelace"
-print(name.title())    # Title will capatilize the first letter of each word in the string. Refered to as 'Title Case'
+# name = "ada lovelace"
+# print(name.title())   
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f'{first_name} {last_name}'
+print(first_name)
+print(last_name)
+print(full_name.title())
+
+message = 'Monty Python'
+print(message.rstrip(' Python'))
+
+message = 'Monty D Python'
+print(message.rstrip(' Python'))
+
+# If you are wanting to just remove the exact word ' Python':
+name = 'Monty Python'
+# if name.endswith(' Python'):
+#     name = message[:-len(' Python')]
+# print(name)
+
+# Or, more cleanly (Python 3.9+)
+print(name.removesuffix(' Python'))
 
