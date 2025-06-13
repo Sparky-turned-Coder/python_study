55ba84c760b7ce3c9f26f703fe7dc80b60e4f425
Chris Jones <cjones@weifieldgroup.com>
2025-06-13 05:54:24 -0500

    first commit to this repo from laptop

--- name.py
+++ name.py
@@ -6,7 +6,11 @@ last_name = 'lovelace'
 full_name = f'{first_name} {last_name}'
 print(first_name)
 print(last_name)
-print(full_name.title())
+print(f"Hello, {full_name.title()}!")
+
+print("What is your name?")
+name = input()
+print(f"Hello there, {name.title()}! Pleasure to meet you!")
 
 message = 'Monty Python'
 print(message.rstrip(' Python'))
@@ -14,12 +18,13 @@ print(message.rstrip(' Python'))
 message = 'Monty D Python'
 print(message.rstrip(' Python'))
 
-# If you are wanting to just remove the exact word ' Python':
+# If you are wanting to just remove the exact word ' Python':   
 name = 'Monty Python'
 # if name.endswith(' Python'):
-#     name = message[:-len(' Python')]
+# name = message[:-len(' Python')]
 # print(name)
 
 # Or, more cleanly (Python 3.9+)
 print(name.removesuffix(' Python'))
+
 
