a1212d811b846367b5f1a2fbd1a5c2161da50d43
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2025-06-15 12:54:50 -0500

    slicing lists

--- slicing.py
+++ slicing.py
@@ -13,4 +13,16 @@ print(a[-2:-4:-1])      # Here we are slicing the list 'a' from index -2 to -4, 
 # Indexes:   0    1    2    3    4
 # Negative: -5   -4   -3   -2   -1
 
+# This means:
+
+# Start at index -2 → 'd'
+
+# Stop before index -4 → 'b'
+
+# Step -1 → move backward one element at a time
+
+# b =   0           1               2            3      4
+
+gaming_systems = ['xbox', 'playstation', 'nintendo switch', 'pc', 'mobile']
+print(gaming_systems[1:3:2])
 
