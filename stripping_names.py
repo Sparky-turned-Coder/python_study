46e968a236a86d20312056a64d359ec49bc2dacb
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2025-06-14 22:29:55 -0500

    Changes and learning

--- stripping_names.py
+++ stripping_names.py
@@ -1,3 +1,7 @@
 print("What is your name, good sir or maam?")
 name = input()
 print(f"\n\t{name}")
+
+print('Who is your favorite person from history?')
+fav_person = input().title()
+print(f"You like {fav_person}? Nice choice. Cool dude.")
